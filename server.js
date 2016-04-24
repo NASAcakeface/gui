4a2069d18a469ff9e8f8f5223605df34ca9301b9
Brandon Ng <ng.bh@hotmail.com>
2016-04-24 11:24:43 -0400

    express node

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var http = require('http'),
     fs = require('fs');
 
+var express = require('express');
+var app = express();
 
-fs.readFile('./index.html', function (err, html) {
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+
+/*// index page 
+app.get('/', function(req, res) {
+    res.render('index');
+});
+*/
+app.listen(8080);
+
+/*fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
@@ -11,5 +25,5 @@ fs.readFile('./index.html', function (err, html) {
         response.write(html);  
         response.end();  
     }).listen(8000);
-});
+});*/
 
